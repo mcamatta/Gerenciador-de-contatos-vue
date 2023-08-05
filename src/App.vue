2903ff71ba00953432a2ed4415aaa4f004121155
mcamatta <camattamichel@gmail.com>
2023-08-04 22:27:22 -0300

    refactor: Removed unnecessary files

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,8 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <RouterLink to="/">Home</RouterLink>
   </nav>
-  <router-view/>
+  <RouterView/>
 </template>
 
 <style>
