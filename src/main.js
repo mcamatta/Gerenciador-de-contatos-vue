84e4d8e2626ea4859fc5c7830dae7b1a22186b56
mcamatta <camattamichel@gmail.com>
2023-08-06 17:45:38 -0300

    chore: remove sweetalert

--- src/main.js
+++ src/main.js
@@ -3,10 +3,6 @@ import App from './App.vue'
 import router from './router'
 import 'bootstrap/dist/css/bootstrap.css'
 
-import VueSweetalert2 from 'vue-sweetalert2';
-import 'sweetalert2/dist/sweetalert2.min.css';
-
 createApp(App)
-  .use(VueSweetalert2)
   .use(router)
   .mount('#app')
