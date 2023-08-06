5dec1d2969fbaac901da3490e82a7d04e4caed0e
mcamatta <camattamichel@gmail.com>
2023-08-05 23:05:14 -0300

    feat: add link do details contact in edit page

--- src/views/ContactEdit.vue
+++ src/views/ContactEdit.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="d-flex justify-content-between mb-3">
     <h2 class="m-0">Contact Edit Form</h2>
-    <RouterLink to="/" class="btn btn-primary fw-bold">Back</RouterLink>
+    <RouterLink :to="{name: 'contact', params: {id: contact.id}}" class="btn btn-primary fw-bold">Back</RouterLink>
   </div>
   <FormContact :contact="contact" @onSubmit="update"/>
 </template>
